7067986c7d18e4581ad5631cb6cca7c6147e4047
BulynkoPasha <119478945+BulynkoPasha@users.noreply.github.com>
2026-08-08 21:32:47 +0300

    feat: add auth service (#12)
    * feat: add DB schema for auth-service
    
    * feat: add JPA entities and repository
    
    * feat: add JWT token provider for auth-service
    
    * feat: add auth service

---
+++ auth-service/src/main/java/com/example/authservice/dto/response/ValidateResponse.java
@@ -0,0 +1,19 @@
+package com.example.authservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateResponse {
+
+    private boolean valid;
+    private Long userId;
+    private String role;
+}
